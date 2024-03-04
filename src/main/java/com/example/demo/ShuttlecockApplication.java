6a65b87977eb8b009e3ba4a5f4b42df0003a687a
park <pdw11@naver.com>
2024-03-04 21:20:10 +0900

    init commit

---
+++ src/main/java/com/example/demo/ShuttlecockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttlecockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttlecockApplication.class, args);
+	}
+
+}
