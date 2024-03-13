0001cc33c60564ac46b0f45ee2365dd747151361
OrengeCake <skid34@naver.com>
2024-03-13 16:52:51 +0900

    feat/jh : 리그 순위

--- src/main/java/com/fp/shuttlecock/leagueboard/LeagueboardRankingController.java
+++ src/main/java/com/fp/shuttlecock/leagueboard/LeagueboardRankingController.java
@@ -1,9 +1,18 @@
 package com.fp.shuttlecock.leagueboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LeagueboardRankingController {
 
+	@Autowired
+	LeagueboardRankingServiceImpl LRS;
+	
+	@GetMapping("/LeagueBoardRanking")
+	public void getLeagueRanking() {
+		
+	}
 	
 }
---
+++ src/main/java/com/fp/shuttlecock/leagueboard/LeagueboardRankingServiceImpl.java
@@ -0,0 +1,20 @@
+package com.fp.shuttlecock.leagueboard;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fp.shuttlecock.user.UserDTO;
+
+@Service
+public class LeagueboardRankingServiceImpl implements LeagueboardRankingService {
+
+	@Autowired
+	LeagueboardRankingMapper LRM;
+	
+	public List<UserDTO> getLeagueRanking() {
+		return LRM.getLeagueRanking();
+	}
+	
+}
